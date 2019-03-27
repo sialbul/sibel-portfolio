b10e8d03316b301b2058b806872117296781d9d4
Sibel <sbaslamisli@yahoo.com>
2019-03-26 22:44:12 -0400

    updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},2000);
+	});
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function menuFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  function filterFunction() {
+	var input, filter, ul, li, a, i;
+	input = document.getElementById("myInput");
+	filter = input.value.toUpperCase();
+	div = document.getElementById("myDropdown").classList.toggle("show");
+	a = div.getElementsByTagName("a");
+	for (i = 0; i < a.length; i++) {
+	  txtValue = a[i].textContent || a[i].innerText;
+	  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		a[i].style.display = "";
+	  } else {
+		a[i].style.display = "none";
+	  }
+	}
+  }
+
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+  function myFunction(x) {
+	if (x.matches) { // If media query matches
+	  $(".dropdown").show();
+	  $("#namePart").hide();
+	  document.getElementById("img").width = "100%";
+	  
+	} else {
+		$(".dropdown").hide();
+		$("#namePart").show();
+	}
+  }
+  
+  var x = window.matchMedia("(max-width: 780px)")
+  myFunction(x) // Call listener function at run time
+  x.addListener(myFunction) // Attach listener function on state changes
+
+
+
+  // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
